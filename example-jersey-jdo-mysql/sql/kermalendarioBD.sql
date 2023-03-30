a2eb66b2e5056f8c8c1ef0b2e5ecb1221b307388
AlvGD <alvaro.gd@opendeusto.es>
2023-03-30 21:22:32 +0200

    BD edit
    cambiado el comentario en bd

--- example-jersey-jdo-mysql/sql/kermalendarioBD.sql
+++ example-jersey-jdo-mysql/sql/kermalendarioBD.sql
@@ -3,7 +3,7 @@ DROP SCHEMA IF EXISTS kermalendarioDB;
 /* DELETE USER 'spq' AT LOCAL SERVER*/
 DROP USER IF EXISTS 'spq'@'localhost';
 
-/* CREATE 'messagesDB' DATABASE */
+/* CREATE 'kermalendarioDB' DATABASE */
 CREATE SCHEMA kermalendarioDB;
 /* CREATE THE USER 'spq' AT LOCAL SERVER WITH PASSWORD 'spq' */
 CREATE USER IF NOT EXISTS 'spq'@'localhost' IDENTIFIED BY 'spq';
