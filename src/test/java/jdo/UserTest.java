f9126b2f273127643d88c8b14f8a8fa0c41eb257
AlvGD <alvaro.gd@opendeusto.es>
2023-04-30 22:26:35 +0200

    tests jdo y pojo
    Realizados los tests de user y userdata
    
    Co-Authored-By: Ker-man <92101004+Ker-man@users.noreply.github.com>

--- src/test/java/jdo/UserTest.java
+++ src/test/java/jdo/UserTest.java
@@ -2,33 +2,45 @@ package jdo;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class UserTest {
+import jdo.User;
 
-	@Test
-	public void testUser() {
-		
-	}
+public class UserTest {
+	
+	private User user;
+	
+	@Before
+    public void setUp() {
+        user = new User("test-login", "test-login", "passwd");
+    }
 
 	@Test
 	public void testGetName() {
-		
+		assertEquals("test-login", user.getName());
+	}
+	
+	@Test
+	public void testGetMail() {
+		assertEquals("test-login", user.getMail());
 	}
 
 	@Test
 	public void testGetPassword() {
-		
+		assertEquals("passwd", user.getPassword());
 	}
 
 	@Test
 	public void testSetPassword() {
-		
+		user.setPassword("newpasswd");
+        assertEquals("newpasswd", user.getPassword());
 	}
 
 	@Test
 	public void testToString() {
-		
+		String expected = "User: name --> test-login, mail -->  test-login";
+		assertEquals(expected, user.toString());
 	}
 
 }
